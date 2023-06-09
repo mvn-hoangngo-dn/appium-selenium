02c1e233b7b8d1bf85910f5c5fa42383d07847d0
duypham-paypay <hoang.ngo@monstar-lab.com>
2023-06-09 17:18:23 +0700

    update

--- Apps/src/test/java/pages/webPages/HomePortalPage.kt
+++ Apps/src/test/java/pages/webPages/HomePortalPage.kt
@@ -24,7 +24,7 @@ class HomePortalPage : BasePage<HomePortalPage>() {
     }
 
     override fun isPageDisplayed(): Boolean? {
-        var txtWelcome = getWebDriver()?.findElement(By.className("welcome-message"))
+        var txtWelcome = getWebDriver()?.findElement(By.className("welcome-messag"))
         return isWebElementDisplayed(txtWelcome)
     }
 
