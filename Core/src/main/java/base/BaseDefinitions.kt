9c63aa9b52c2defd6c7b1fbd3cb0f5d17880cca2
duypham-paypay <hoang.ngo@monstar-lab.com>
2023-06-10 10:33:57 +0700

    update

--- Core/src/main/java/base/BaseDefinitions.kt
+++ Core/src/main/java/base/BaseDefinitions.kt
@@ -1,33 +1,33 @@
-//package base
-//
-//import core.AppiumBaseClass
-//import core.AppiumController
-//import core.PageFactory
-//import io.cucumber.java8.En
-//import io.cucumber.java8.Scenario
-//import org.openqa.selenium.OutputType
-//import org.openqa.selenium.TakesScreenshot
-//import org.openqa.selenium.remote.RemoteWebDriver
-//import org.openqa.selenium.support.PageFactory.initElements
-//import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory
-//
-///**
-// * @author tien.hoang
-// */
-//abstract class BaseDefinitions : AppiumBaseClass(), En {
-//
-//
-//    fun getWebDriver(): RemoteWebDriver? {
-//        return AppiumController.instance.getWebDriver()
-//    }
-//
-//    fun quitWebDriver() {
-//        AppiumController.instance.quitWebDriver()
-//    }
-//
-//    protected fun embedScreenshot(scenario: Scenario) {
-//        val ts = AppiumController.instance.getDriver() as TakesScreenshot
-//        val src = ts.getScreenshotAs(OutputType.BYTES)
-//        scenario.attach(src, "image/png", "Screenshot: " + scenario.name)
-//    }
-//}
+package base
+
+import core.AppiumBaseClass
+import core.AppiumController
+import core.PageFactory
+import io.cucumber.java8.En
+import io.cucumber.java8.Scenario
+import org.openqa.selenium.OutputType
+import org.openqa.selenium.TakesScreenshot
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.support.PageFactory.initElements
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory
+
+/**
+ * @author tien.hoang
+ */
+abstract class BaseDefinitions : AppiumBaseClass(), En {
+
+
+    fun getWebDriver(): RemoteWebDriver? {
+        return AppiumController.instance.getWebDriver()
+    }
+
+    fun quitWebDriver() {
+        AppiumController.instance.quitWebDriver()
+    }
+
+    protected fun embedScreenshot(scenario: Scenario) {
+        val ts = AppiumController.instance.getDriver() as TakesScreenshot
+        val src = ts.getScreenshotAs(OutputType.BYTES)
+        scenario.attach(src, "image/png", "Screenshot: " + scenario.name)
+    }
+}
