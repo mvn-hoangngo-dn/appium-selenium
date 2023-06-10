c45dcc32ffdacaca149d45906e9aa2ab97b27c01
duypham-paypay <hoang.ngo@monstar-lab.com>
2023-06-10 11:22:40 +0700

    update

--- Apps/src/test/java/pages/appPages/DragPageAndroid.kt
+++ Apps/src/test/java/pages/appPages/DragPageAndroid.kt
@@ -6,7 +6,6 @@ class DragPageAndroid: DragPage() {
         println("Handling for Android")
         dragAction(dragC1,dropC1)
         dragAction(dragL1,dropL1)
-        dragAction(dragC3,dropC3)
         return this
     }
 
--- Apps/src/test/java/stepdefs/GlobalHooks.kt
+++ Apps/src/test/java/stepdefs/GlobalHooks.kt
@@ -1,20 +1,20 @@
-//package stepdefs
-//
-//import base.BaseDefinitions
-//import io.cucumber.java8.Scenario
-//
-//class GlobalHooks : BaseDefinitions() {
-//
-//    init {
-//        Before(0) { _: Scenario ->
-//            // Run with junit
-//        }
-//
-//        After(0) { scenario: Scenario ->
-//            if (scenario.isFailed) {
-//                println("Taking screenshots...")
-//                embedScreenshot(scenario)
-//            }
-//        }
-//    }
-//}
+package stepdefs
+
+import base.BaseDefinitions
+import io.cucumber.java8.Scenario
+
+class GlobalHooks : BaseDefinitions() {
+
+    init {
+        Before(0) { _: Scenario ->
+            // Run with junit
+        }
+
+        After(0) { scenario: Scenario ->
+            if (scenario.isFailed) {
+                println("Taking screenshots...")
+                embedScreenshot(scenario)
+            }
+        }
+    }
+}
